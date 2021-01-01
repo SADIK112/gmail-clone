b32a878c5633d825bf2f4a517b2b5d0ae1242fd4
SADIK112 <35666212+SADIK112@users.noreply.github.com>
2021-01-01 12:14:50 +0600

    section created

--- src/emailList.js
+++ src/emailList.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import './emailList.css'
+import Section from "./sections";
 import { Checkbox, IconButton } from '@material-ui/core'
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import RedoIcon from '@material-ui/icons/Redo';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import KeyboardHideIcon from '@material-ui/icons/KeyboardHide';
+import SettingsIcon from '@material-ui/icons/Settings';
+import InboxIcon from '@material-ui/icons/Inbox';
+import PeopleIcon from '@material-ui/icons/People';
+import LocalOfferIcon from '@material-ui/icons/LocalOffer';
 
 function EmailList() {
     return (
@@ -21,7 +29,28 @@ function EmailList() {
                         <MoreVertIcon />
                     </IconButton>
                 </div>
+
+                <div className="emailList__settingsRight">
+                    <IconButton>
+                        <ChevronLeftIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChevronRightIcon />
+                    </IconButton>
+                    <IconButton>
+                        <KeyboardHideIcon />
+                    </IconButton>
+                    <IconButton>
+                        <SettingsIcon />
+                    </IconButton>
+                </div>
             </div>
+            
+            <div className="emailList__sections">
+                    <Section Icon={InboxIcon} title="primary" color="red" selected />
+                    <Section Icon={PeopleIcon} title="Socials" color="#1A73E8"  />
+                    <Section Icon={LocalOfferIcon} title="Promotions" color="green"  />
+                </div>
         </div>
     )
 }
